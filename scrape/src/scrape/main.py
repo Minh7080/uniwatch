9c166812037fa0e3b1a8a609b37b99eee8f6ac26
Minh7080 <minhtuannguyen642005@gmail.com>
2025-11-05 04:27:21 +1100

    feat: Move from poetry to just pip

--- scrape/src/scrape/main.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv, find_dotenv
-from scrape.scrape import scrape_subs
-import asyncio
-
-load_dotenv(find_dotenv())
-
-def main():
-    asyncio.run(scrape_subs(['testingground4bots', 'unsw', 'Fishdom']))
