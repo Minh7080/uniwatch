65b342c0ec9300bdb5f6f5e72ccd13e2068f728d
Minh7080 <minhtuannguyen642005@gmail.com>
2025-12-19 00:02:59 +1100

    feat: Update main

--- scrape/src/main.py
+++ scrape/src/main.py
@@ -1,7 +1,56 @@
 from dotenv import load_dotenv, find_dotenv
 from scrape import scrape_subs
 import asyncio
+from classifier import classify_posts
 
 load_dotenv(find_dotenv())
 
-asyncio.run(scrape_subs(['testingground4bots', 'unsw', 'usyd', 'UTS', 'UOW', 'Anu']))
+print('Scraping...')
+
+asyncio.run(scrape_subs([
+    'UniAdelaide',
+    'australiancatholicuni',
+    'Anu',
+    'CharlesDarwin',
+    'CharlesSturtUni',
+    'CQUni',
+    'curtin',
+    'deakin',
+    'edithcowan',
+    'FedUni',
+    'flindersuni',
+    'GriffithUni',
+    'JamesCookUniversity',
+    'LaTrobe',
+    'MacUni',
+    'murdoch', # yikes
+    'QUTreddit',
+    'rmit',
+    'SCU', # Southern Cross University
+    'swinburne',
+    'TorrensUni',
+    'universityofcanberra',
+    'unimelb',
+    'UNEAustralia',
+    'unsw', 
+    'UoNau',
+    'UQreddit',
+    'UniversityofSA',
+    'UniSQ',
+    'usyd', 
+    'UTAS',
+    'UTS', 
+    'UniSC',
+    'uwa',
+    'vicuni',
+    'UOW', 
+    'UWS',
+]))
+
+print('Scraping done')
+
+print('Classifying')
+
+asyncio.run(classify_posts());
+
+print('Classifying done')
