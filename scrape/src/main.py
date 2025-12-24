bec4048b016d1296a5077aeafeb0d7fe0c93ff58
Minh7080 <minhtuannguyen642005@gmail.com>
2025-12-24 18:11:49 +1100

    Initialize project using create-express-typescript-application

--- scrape/src/main.py
+++ scrape/src/main.py
@@ -2,50 +2,20 @@ from dotenv import load_dotenv, find_dotenv
 from scrape import scrape_subs
 import asyncio
 from classifier import classify_posts
+from pathlib import Path
+import json
 
 load_dotenv(find_dotenv())
 
 print('Scraping...')
 
-asyncio.run(scrape_subs([
-    'UniAdelaide',
-    'australiancatholicuni',
-    'Anu',
-    'CharlesDarwin',
-    'CharlesSturtUni',
-    'CQUni',
-    'curtin',
-    'deakin',
-    'edithcowan',
-    'FedUni',
-    'flindersuni',
-    'GriffithUni',
-    'JamesCookUniversity',
-    'LaTrobe',
-    'MacUni',
-    'murdoch', # yikes
-    'QUTreddit',
-    'rmit',
-    'SCU', # Southern Cross University
-    'swinburne',
-    'TorrensUni',
-    'universityofcanberra',
-    'unimelb',
-    'UNEAustralia',
-    'unsw', 
-    'UoNau',
-    'UQreddit',
-    'UniversityofSA',
-    'UniSQ',
-    'usyd', 
-    'UTAS',
-    'UTS', 
-    'UniSC',
-    'uwa',
-    'vicuni',
-    'UOW', 
-    'UWS',
-]))
+BASE_DIR = Path(__file__).resolve().parent
+SUBREDDITS_JSON_PATH = BASE_DIR.parents[1] / 'subreddits.json'
+
+with open(SUBREDDITS_JSON_PATH, 'r', encoding='utf-8') as file:
+    subreddits = json.load(file)
+
+asyncio.run(scrape_subs(subreddits))
 
 print('Scraping done')
 
