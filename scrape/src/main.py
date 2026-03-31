38624df436b56b8bdef3482f2f9a67335e48ae2d
Minh7080 <minhtuannguyen642005@gmail.com>
2026-03-31 19:30:40 +1100

    feat: Use schedule to keep the task alive and run for every hour
    This remove the need for cron job which is better for project running on
    a docker container

--- scrape/src/main.py
+++ scrape/src/main.py
@@ -1,26 +1,28 @@
-from dotenv import load_dotenv, find_dotenv
 from scrape import scrape_subs
 import asyncio
 from classifier import classify_posts
 from pathlib import Path
 import json
-
-load_dotenv(find_dotenv())
-
-print('Scraping...')
+import schedule
+import time
 
 BASE_DIR = Path(__file__).resolve().parent
-SUBREDDITS_JSON_PATH = BASE_DIR.parents[1] / 'subreddits.json'
+SUBREDDITS_JSON_PATH = BASE_DIR.parents[1] / 'shared/subreddits.json'
 
 with open(SUBREDDITS_JSON_PATH, 'r', encoding='utf-8') as file:
     subreddits = json.load(file)
 
-asyncio.run(scrape_subs(subreddits))
+def job():
+    print('Scraping...')
+    asyncio.run(scrape_subs([s['subreddit'] for s in subreddits]))
+    print('Scraping done')
+    print('Classifying...')
+    asyncio.run(classify_posts())
+    print('Classifying done')
 
-print('Scraping done')
+job()
+schedule.every(1).hours.do(job)
 
-print('Classifying')
-
-asyncio.run(classify_posts());
-
-print('Classifying done')
+while True:
+    schedule.run_pending()
+    time.sleep(60)
