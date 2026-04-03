1387138d431cf1af0b31fda0eaaf2c8f3ebe9dd5
Minh7080 <minhtuannguyen642005@gmail.com>
2026-04-03 20:43:54 +1100

    feat: Update posts context to include cursor and isLoading
    a

--- next/app/context/posts-context.tsx
+++ next/app/context/posts-context.tsx
@@ -4,16 +4,37 @@ import { createContext, useContext, useState } from "react";
 import { ResponseView as Post } from "@/utils/dbTypes";
 
 type PostsContextType = {
-  posts: Post[] | null;
-  setPosts: React.Dispatch<React.SetStateAction<Post[] | null>>;
+  posts: {
+    get: Post[] | null,
+    set: React.Dispatch<React.SetStateAction<Post[] | null>>,
+  }
+
+  cursor: {
+    get: string | null,
+    set: React.Dispatch<React.SetStateAction<string | null>>;
+  }
+
+  isLoading: {
+    get: boolean,
+    set: React.Dispatch<React.SetStateAction<boolean>>;
+  }
 };
 
 const PostsContext = createContext<PostsContextType | null>(null);
 
 export function PostsProvider({ children }: { children: React.ReactNode }) {
-  const [posts, setPosts] = useState<Post[] | null>([]);
+  const [posts, setPosts] = useState<Post[] | null>(null);
+  const [cursor, setCursor] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const value: PostsContextType = {
+    posts:     { get: posts,     set: setPosts },
+    cursor:    { get: cursor,    set: setCursor },
+    isLoading: { get: isLoading, set: setIsLoading },
+  };
+
   return (
-    <PostsContext.Provider value={{ posts, setPosts }}>
+    <PostsContext.Provider value={value}>
       {children}
     </PostsContext.Provider>
   );
